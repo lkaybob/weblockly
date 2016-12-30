a90b765cbba46b6b31f87a24ab3a8bf968dc6587
Lawernce Kay <lkay9495@hotmail.com>
2016-12-30 15:56:27 +0900

    Changed workspcae & added static asset path * app.js : Added node_module/blockly as a static path for Blockly JS Library * index.pug : Deleted blocklyArea to change workspace as fixed size & added textarea to show generated code from blocks * Still have an issue (Basic blocks not showing)

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'node_modules/blockly')));
 app.use('/', index);
 app.use('/users', users);
 
