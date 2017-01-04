05cef9f9ed102cafa99b8205b98a9f4f899c2f7c
credtiger96 <credtiger96@gmail.com>
2017-01-04 19:41:56 +0900

    Update workspaceManager.js
    added comment.

--- public/javascripts/blockly/workspaceManager.js
+++ public/javascripts/blockly/workspaceManager.js
@@ -1,33 +1,44 @@
 /**
  * Created by credtiger96 on 17. 1. 2.
  */
+
+/* 
+WorkspaceManager is Namespace that manage mode. 
+each mode has it's own workspace. 
+
+* Global mode : Global (not on sepecific elements) Code   
+* ID mode : Specific Element's Code. 
+ 
+*/
 var WorkspaceManager = {
     init : function () {
         this.globalWorkSpace = workspace;
         this.idWorkspace = [];
-        this.classWorkspace = [];
+        //this.classWorkspace = [];
         this.currentMode = this.MODE_GLOBAL;
     },
     currentMode : null,
     currentID : null,
-    currentClass : null,
+    //currentClass : null,
+    
+    /*change to Global mode */ 
     changeToGlobalMode : function () {
 
         alertModeChange('Global');
 
         workspace.updateToolbox(globalToolbox);
-        //alert('global');
+        
         this.currentMode = this.MODE_GLOBAL;
         workspace.clear();
         Blockly.Xml.domToWorkspace(this.globalWorkSpace,workspace);
 
     },
+    /*Change to mode that treat element that has specific ID */ 
     changeToIdMode : function (id) {
         alertModeChange('ID : ' + id);
 
         workspace.updateToolbox(idToolbox);
 
-        //alert('id : ' + id );
         if (! this.idWorkspace[id])
             this.idWorkspace[id] = this.createNewWorkspace();
         this.currentMode = this.MODE_ID;
@@ -41,8 +52,9 @@ var WorkspaceManager = {
         //console.log('created');
         return Blockly.Xml.textToDom('<xml></xml>');
 
-    }
-    ,
+    },
+    
+    /* Before Change mode, currente state must be saved. */ 
     saveCurrnetState : function () {
         //console.log('current state :', this.currentMode);
         switch (this.currentMode){
@@ -54,16 +66,18 @@ var WorkspaceManager = {
                 break;
         }
     },
-    saveCurrentClassWorkspace: function (className) {
-
-    },
+    
     // Constants
     MODE_GLOBAL : 'global',
     MODE_ID : 'id',
     MODE_CLASS : 'class'
 };
-//
-// changeMode event handler
+/*
+ ChangeMode event handler
+ 
+ if tagId is not set, mode is set to Global. 
+ else mode is set to specific id mode. 
+ */
 $(document).on('ModeChange', function(event) {
     //console.log(event);
     if (event.tagId){
